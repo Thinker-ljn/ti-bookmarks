a0d9c51ba6375ad46fa8e473a83f5196393cdc10
thinker <thinker_ljn@163.com>
2018-09-13 23:03:11 +0800

    fix

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,14 +1,3 @@
-var fs = require('fs');
-
-var readFileThunk = function(src) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
-      if(err) return reject(err);
-      resolve(data);
-    });
-  });
-}
-
 const routes = [
   {
     p: '/api/bookmarks',
