1c8e200f0b86b4641e01bc6767fbdd1f6aba93d2
thinker <thinker_ljn@163.com>
2018-11-26 14:02:13 +0800

    add table spaced_repetition to database

---
+++ server/src/database/migrations/spaced_repetition.migration.js
@@ -0,0 +1,11 @@
+module.exports = `
+  CREATE TABLE IF NOT EXISTS spaced_repetition (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL DEFAULT 'default',
+  model_type varchar(255) NOT NULL,
+  model_id int(11) NOT NULL,
+  execute_time timestamp NOT NULL,
+  created_at timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8
+`
