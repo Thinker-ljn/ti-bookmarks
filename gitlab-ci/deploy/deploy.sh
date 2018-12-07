4d19f2f56f2e33a9b418243243b17204459cf497
Thinker-ljn <thinker_ljn@163.com>
2018-12-07 15:00:56 +0800

    update deploy.sh

--- gitlab-ci/deploy/deploy.sh
+++ gitlab-ci/deploy/deploy.sh
@@ -2,6 +2,7 @@
 sudo docker-compose down
 sudo docker login registry.gitlab.com -u gitlab+deploy-token-30320 -p eTR9PXMSs-41YrxgixZz
 sudo docker pull registry.gitlab.com/thinker-ljn/ti:latest
+sudo docker rmi $(docker images | grep "<none>" | awk '{ print $3 }')
 sudo docker-compose build node
 sudo docker-compose run node npm run tool migrate
 sudo docker-compose down
