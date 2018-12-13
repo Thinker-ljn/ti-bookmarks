376373f8232c4d1df8a56a3e71a5a06fb1c536a2
Thinker-ljn <thinker_ljn@163.com>
2018-12-13 17:47:07 +0800

    entry

---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+require('module-alias/register')
+require('dotenv').config()
+String.prototype.plural = require('@core/lib/plural')
+const path = require('path')
+
+const Koa = require('koa')
+const serve = require('koa-static')
+const app = new Koa()
+const router = require('./router')
+const bodyParser = require('koa-bodyparser')
+
+app.use(function (ctx, next) {
+  if (ctx.method !== 'OPTIONS') {
+    return next();
+  }
+  if (!ctx.get('Access-Control-Request-Method')) {
+    // this not preflight request, ignore it
+    return next();
+  }
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  ctx.status = 204;
+});
+
+app.use(serve(path.join(__dirname, '..', 'public')))
+app.use(bodyParser())
+app.use(router.routes())
+
+module.exports = app
