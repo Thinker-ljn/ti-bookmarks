a90100c0cc3a352bba4cf32f645a97ef34d7888d
Thinker-ljn <thinker_ljn@163.com>
2018-12-13 17:47:26 +0800

    controller validator

--- server/src/core/controller/index.js
+++ server/src/core/controller/index.js
@@ -1,8 +1,18 @@
+const Parameter = require('parameter')
 class Controller {
-    constructor () {
-  
+  constructor (ctx) {
+    this.validator = new Parameter({
+      validateRoot: true
+    })
+    this.ctx = ctx
+  }
+
+  validate (rule, data) {
+    let error = this.validator.validate(rule, data)
+    if (error) {
+      this.ctx.throw(422, JSON.stringify(error))
     }
   }
-  
-  module.exports = Controller
-  
+}
+
+module.exports = Controller
