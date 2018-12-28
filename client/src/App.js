63cc358f81ee5f944aff836e62b7d4eee6164dbd
Thinker-ljn <thinker_ljn@163.com>
2018-12-28 16:53:55 +0800

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,9 @@ class App extends Component {
 
   addBookmarkToBrowser (e) {
     e.preventDefault()
-    let url = 'localhost:8001'
-    let exec = `(function(){
-      var h = window.screen.availHeight;var w = window.screen.availWidth;window.open("http://${url}/add-bookmark?u="+encodeURIComponent(window.location.href)+"&t="+encodeURIComponent(document.title),"添加书签","height=340,width="+380+",top="+(h/2-165)+",left="+(w/2-185)+",toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no, status=no");})()`
+    let {port, host} = location
+    let url = host + port ? `:${port}` : ''
+    let exec = `(function(){var h = window.screen.availHeight;var w = window.screen.availWidth;window.open("http://${url}/add-bookmark?u="+encodeURIComponent(window.location.href)+"&t="+encodeURIComponent(document.title),"添加书签","height=340,width="+380+",top="+(h/2-165)+",left="+(w/2-185)+",toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no, status=no");})()`
     let string = 'javascript:eval(decodeURIComponent(' + encodeURIComponent(exec) + '))'
     console.log(string)
   }
