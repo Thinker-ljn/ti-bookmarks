f7970342983d2f827c278474232ca5557f48e747
Thinker-ljn <thinker_ljn@163.com>
2019-01-18 16:48:01 +0800

    quick add

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,6 @@ const { Footer } = Layout
 class App extends Component {
   constructor(props) {
     super(props)
-    this.addBookmarkToBrowser = this.addBookmarkToBrowser.bind(this)
-  }
-
-  addBookmarkToBrowser (e) {
-    e.preventDefault()
-    let {port, host} = location
-    let url = host + port ? `:${port}` : ''
-    let exec = `(function(){var h = window.screen.availHeight;var w = window.screen.availWidth;window.open("http://${url}/add-bookmark?u="+encodeURIComponent(window.location.href)+"&t="+encodeURIComponent(document.title),"添加书签","height=340,width="+380+",top="+(h/2-165)+",left="+(w/2-185)+",toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no, status=no");})()`
-    let string = 'javascript:eval(decodeURIComponent(' + encodeURIComponent(exec) + '))'
-    console.log(string)
   }
 
   render() {
--- client/src/components/Content/index.js
+++ client/src/components/Content/index.js
@@ -28,9 +28,18 @@ class AppContent extends Component {
     })
   }
 
+  getQuickAddBookmark () {
+    let {port, host, protocol} = location
+    let url = protocol + '//' + host + (port ? `:${port}` : '')
+    console.log(location, port, host, protocol, url)
+    let exec = `(function(){var h = window.screen.availHeight;var w = window.screen.availWidth;window.open("${url}/add-bookmark?u="+encodeURIComponent(window.location.href)+"&t="+encodeURIComponent(document.title),"添加书签","height=340,width="+380+",top="+(h/2-165)+",left="+(w/2-185)+",toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no, status=no");})()`
+    let string = 'javascript:eval(decodeURIComponent(' + encodeURIComponent(exec) + '))'
+    return string
+  }
+
   render () {
     const { bookmarks } = this.state
-
+    let quickAdd = this.getQuickAddBookmark()
     const loopBk = function () {
       return bookmarks.map(bookmark => {
         return (
@@ -40,7 +49,7 @@ class AppContent extends Component {
     }
     return (
       <Layout styleName="content">
-        <h3 styleName="title">所有书签</h3>
+        <h3 styleName="title">所有书签<a styleName="quick-add" href={quickAdd} title="拖拽至浏览器书签栏">快捷添加书签</a></h3>
         <ul>
           {loopBk()}
         </ul>
